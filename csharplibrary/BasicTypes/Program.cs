2c1ac4a6f977c333cf6de24a8752b48cb3c1bc4f
Angela Reid <reidang94@gmail.com>
2016-07-25 10:21:41 -0400

    add csharplibrary and mvc games

---
+++ csharplibrary/BasicTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicTypes
+{
+    class Program
+    {
+        //String data type demo
+        static void Main(string[] args)
+        {
+           string animal = "Dog";
+           string name = "Nicole";
+           string activity = "walk";
+
+            var sentence = $" {name} + {animal} + {activity}";
+
+            Console.WriteLine($" {name} takes her {animal} for a {activity}.");
+              Console.ReadLine();               
+        }
+    }
+   
+}
+
